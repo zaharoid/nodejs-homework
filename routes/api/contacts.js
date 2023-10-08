f7de11aa9b9ace3f876b00fa5b55d1ee3c170ac8
zaharoid <gololobov.zahar23@gmail.com>
2023-10-08 21:45:19 +0200

    h

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -8,34 +8,33 @@ import {
   validateSchema,
   contactUpdateFavoriteSchema,
 } from "../../models/Contact.js";
-// import ctrl from "../../controllers/movies-controller.js";
-
-// const contactAddValidate = validationBody(validateSchema);
-// const contactUpdateFavoriteValidate = validationBody(
-//   contactUpdateFavoriteSchema
-// );
+import ctrl from "../../controllers/movies-controller";
+const contactAddValidate = validationBody(validateSchema);
+const contactUpdateFavoriteValidate = validationBody(
+  contactUpdateFavoriteSchema
+);
 const contactsRouter = express.Router();
 
-// contactsRouter.get("/", ctrl.getAllContacts);
+contactsRouter.get("/", ctrl.getAllContacts);
 
-// contactsRouter.get("/:id", isValidId, ctrl.getContactById);
+contactsRouter.get("/:id", isValidId, ctrl.getContactById);
 
-// contactsRouter.post("/", isEmptyBody, contactAddValidate, ctrl.createContact);
+contactsRouter.post("/", isEmptyBody, contactAddValidate, ctrl.createContact);
 
-// contactsRouter.delete("/:id", isValidId, ctrl.deleteContact);
+contactsRouter.delete("/:id", isValidId, ctrl.deleteContact);
 
-// contactsRouter.put(
-//   "/:id",
-//   isValidId,
-//   isEmptyBody,
-//   contactAddValidate,
-//   ctrl.updateContact
-// );
-// contactsRouter.patch(
-//   "/:id/favorite",
-//   isValidId,
-//   contactUpdateFavoriteValidate,
-//   ctrl.updateStatusContact
-// );
+contactsRouter.put(
+  "/:id",
+  isValidId,
+  isEmptyBody,
+  contactAddValidate,
+  ctrl.updateContact
+);
+contactsRouter.patch(
+  "/:id/favorite",
+  isValidId,
+  contactUpdateFavoriteValidate,
+  ctrl.updateStatusContact
+);
 
 export default contactsRouter;
