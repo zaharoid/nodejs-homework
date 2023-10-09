e467c6769d26f304ba5ac35790940351c2a63262
zaharoid <gololobov.zahar23@gmail.com>
2023-10-09 13:42:13 +0200

    removed controllers

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,3 @@
-// const addContactValidation = require("./validateBody");
-
-// module.exports = addContactValidation;
-
 export { default as validationBody } from "./validateBody.js";
 export { default as isValidId } from "./isValidId.js";
 export { default as isEmptyBody } from "./isEmptyBody.js";
